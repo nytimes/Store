3860b7145644c46e5c61fc71601bfc598e50a3b4
digitalbuddha <mnakhimovich@gmail.com>
2016-11-29 15:11:04 -0500

    start no sample activity

--- app/src/main/java/com/nytimes/android/sample/activity/StoreActivity.java
+++ app/src/main/java/com/nytimes/android/sample/activity/StoreActivity.java
@@ -4,8 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.nytimes.android.store.base.Persister;
 import com.nytimes.android.store.base.Store;
+import com.nytimes.android.store.base.impl.BarCode;
 import com.nytimes.android.store.base.impl.StoreBuilder;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
 
 import static com.nytimes.android.store.base.impl.BarCode.empty;
 
@@ -13,12 +21,39 @@ public class StoreActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AtomicInteger integer = new AtomicInteger(0);
         Store<String> sampleStore = StoreBuilder.<String>builder()
                 .nonObservableFetcher(barCode -> "Hello")
+                .persister(new Persister<String>() {
+                    @Override
+                    public Observable<String> read(BarCode barCode) {
+                        return Observable.just(integer.incrementAndGet() + "");
+                    }
+
+                    @Override
+                    public Observable<Boolean> write(BarCode barCode, String s) {
+                        return Observable.empty();
+                    }
+                })
                 .open();
 
-        sampleStore.get(empty()).subscribe(hello -> {
-            Toast.makeText(this,hello,Toast.LENGTH_SHORT).show();
-        });
+        sampleStore.get(empty())
+                .delay(5, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(hello -> {
+                    Toast.makeText(this, hello, Toast.LENGTH_SHORT).show();
+                });
+        sampleStore.get(empty())
+                .delay(10, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(hello -> {
+                    Toast.makeText(this, hello, Toast.LENGTH_SHORT).show();
+                });
+        sampleStore.get(empty())
+                .delay(15, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(hello -> {
+                    Toast.makeText(this, hello, Toast.LENGTH_SHORT).show();
+                });
     }
 }
