135f8a2155321e685028dc56a8ebd3b651f369de
Brian Plummer <brian.r.plummer@gmail.com>
2016-12-05 23:29:33 -0500

    ui portion, reecyclerview, adapter, viewholder

--- app/src/main/java/com/nytimes/android/sample/activity/StoreActivity.java
+++ app/src/main/java/com/nytimes/android/sample/activity/StoreActivity.java
@@ -1,7 +1,10 @@
 package com.nytimes.android.sample.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,11 +14,13 @@ import com.nytimes.android.external.store.base.Store;
 import com.nytimes.android.external.store.base.impl.BarCode;
 import com.nytimes.android.external.store.base.impl.StoreBuilder;
 import com.nytimes.android.sample.BuildConfig;
+import com.nytimes.android.sample.R;
 import com.nytimes.android.sample.data.model.Children;
 import com.nytimes.android.sample.data.model.GsonAdaptersModel;
 import com.nytimes.android.sample.data.model.Post;
 import com.nytimes.android.sample.data.model.RedditData;
 import com.nytimes.android.sample.data.remote.Api;
+import com.nytimes.android.sample.reddit.PostAdapter;
 
 import java.util.List;
 
@@ -29,10 +34,22 @@ import rx.schedulers.Schedulers;
 import static android.widget.Toast.makeText;
 
 
-public class StoreActivity extends Activity {
+public class StoreActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        postAdapter = new PostAdapter();
+        recyclerView = (RecyclerView) findViewById(R.id.postRecyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(postAdapter);
 
         loadPosts();
     }
@@ -51,6 +68,7 @@ public class StoreActivity extends Activity {
     }
 
     private void showPosts(List<Post> posts) {
+        postAdapter.setPosts(posts);
         makeText(StoreActivity.this,
                 "Loaded " + posts.size() + " posts",
                 Toast.LENGTH_SHORT)
--- app/src/main/java/com/nytimes/android/sample/data/model/Post.java
+++ app/src/main/java/com/nytimes/android/sample/data/model/Post.java
@@ -2,6 +2,8 @@ package com.nytimes.android.sample.data.model;
 
 import android.support.annotation.Nullable;
 
+import com.google.common.base.Optional;
+
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -18,5 +20,12 @@ public abstract class Post {
 
     @Nullable
     public abstract Integer width();
+
+    @Value.Derived
+    public Optional<Image> nestedThumbnail() {
+        if (preview() == null || preview().images() == null || preview().images().get(0).source() == null)
+            return Optional.absent();
+        return Optional.of(preview().images().get(0).source());
+    }
 
 }
