a990c6982f813e858fd4fa41d4c6298f4574f074
Maksim Moiseikin <m.moiseikin@gmail.com>
2017-01-14 13:14:23 +0100

    Implement Moshi Reader parser

--- middleware-moshi/src/main/java/com/nytimes/android/external/store/middleware/moshi/MoshiParserFactory.java
+++ middleware-moshi/src/main/java/com/nytimes/android/external/store/middleware/moshi/MoshiParserFactory.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import com.nytimes.android.external.store.base.Parser;
 import com.squareup.moshi.Moshi;
 
+import java.io.Reader;
 import java.lang.reflect.Type;
 
 import okio.BufferedSource;
@@ -63,5 +64,29 @@ public final class MoshiParserFactory {
     @NonNull
     public static <T> Parser<BufferedSource, T> createSourceParser(@NonNull Type type) {
         return createSourceParser(new Moshi.Builder().build(), type);
+    }
+
+    /**
+     * Returns a new Parser which parses from {@link Reader} to the specified type, using
+     * the provided {@link Moshi} instance.
+     */
+    @NonNull
+    public static <T> Parser<Reader, T> createReaderParser(@NonNull Moshi moshi, @NonNull Type type) {
+        if (moshi == null) {
+            throw new IllegalArgumentException("moshi cannot be null.");
+        }
+        if (type == null) {
+            throw new IllegalArgumentException("type cannot be null.");
+        }
+        return new MoshiReaderParser<>(moshi, type);
+    }
+
+    /**
+     * Returns a new Parser which parses from {@link Reader} to the specified type, using
+     * a new default configured {@link Moshi} instance.
+     */
+    @NonNull
+    public static <T> Parser<Reader, T> createReaderParser(@NonNull Type type) {
+        return createReaderParser(new Moshi.Builder().build(), type);
     }
 }
---
+++ middleware-moshi/src/test/java/com/nytimes/android/external/store/middleware/moshi/MoshiReaderParserTest.java
@@ -0,0 +1,99 @@
+package com.nytimes.android.external.store.middleware.moshi;
+
+import com.nytimes.android.external.store.base.Fetcher;
+import com.nytimes.android.external.store.base.Parser;
+import com.nytimes.android.external.store.base.Persister;
+import com.nytimes.android.external.store.base.Store;
+import com.nytimes.android.external.store.base.impl.BarCode;
+import com.nytimes.android.external.store.base.impl.ParsingStoreBuilder;
+import com.nytimes.android.external.store.middleware.moshi.data.Foo;
+import com.squareup.moshi.Moshi;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+import rx.Observable;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MoshiReaderParserTest {
+    private static final String KEY = "key";
+    private static final String sourceString =
+            "{\"number\":123,\"string\":\"abc\",\"bars\":[{\"string\":\"def\"},{\"string\":\"ghi\"}]}";
+
+    @Mock
+    Fetcher<Reader> fetcher;
+    @Mock
+    Persister<Reader> persister;
+
+    private final BarCode barCode = new BarCode("value", KEY);
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        Reader reader = new StringReader(sourceString);
+        assertNotNull(reader);
+
+        when(fetcher.fetch(barCode))
+                .thenReturn(Observable.just(reader));
+
+        when(persister.read(barCode))
+                .thenReturn(Observable.<Reader>empty())
+                .thenReturn(Observable.just(reader));
+
+        when(persister.write(barCode, reader))
+                .thenReturn(Observable.just(true));
+    }
+
+    @Test
+    public void testReaderParser() throws Exception {
+
+        Parser<Reader, Foo> parser = MoshiParserFactory.createReaderParser(Foo.class);
+
+        Store<Foo> store = ParsingStoreBuilder.<Reader, Foo>builder()
+                .persister(persister)
+                .fetcher(fetcher)
+                .parser(parser)
+                .open();
+
+        Foo result = store.get(barCode).toBlocking().first();
+
+        assertEquals(result.number, 123);
+        assertEquals(result.string, "abc");
+        assertEquals(result.bars.size(), 2);
+        assertEquals(result.bars.get(0).string, "def");
+        assertEquals(result.bars.get(1).string, "ghi");
+
+        verify(fetcher, times(1)).fetch(barCode);
+
+    }
+
+    @Test
+    public void testInvalidArgumentsInFactory() {
+        try {
+            MoshiParserFactory.createReaderParser(null, Foo.class);
+            fail();
+        } catch (IllegalArgumentException expected) {
+            assertEquals("moshi cannot be null.", expected.getMessage());
+        }
+
+        try {
+            MoshiParserFactory.createReaderParser(new Moshi.Builder().build(), null);
+            fail();
+        } catch (IllegalArgumentException expected) {
+            assertEquals("type cannot be null.", expected.getMessage());
+        }
+    }
+
+}
