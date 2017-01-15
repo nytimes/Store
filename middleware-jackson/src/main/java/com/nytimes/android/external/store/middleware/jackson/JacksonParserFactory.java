966462fb0e403981df1cd93316f3d93f0bcabd56
Maksim Moiseikin <m.moiseikin@gmail.com>
2017-01-15 14:31:19 +0100

    Create Jackson BufferedSource parser

--- middleware-jackson/src/main/java/com/nytimes/android/external/store/middleware/jackson/JacksonParserFactory.java
+++ middleware-jackson/src/main/java/com/nytimes/android/external/store/middleware/jackson/JacksonParserFactory.java
@@ -9,6 +9,8 @@ import com.nytimes.android.external.store.base.Parser;
 
 import java.lang.reflect.Type;
 
+import okio.BufferedSource;
+
 /**
  * Factory which returns various Jackson {@link Parser} implementations.
  */
@@ -41,10 +43,43 @@ public final class JacksonParserFactory {
 
     /**
      * Returns a new Parser which parses from a String to the specified type, using
-     * a new default {@link JsonFactory} instance.
+     * a new default {@link ObjectMapper} instance.
      */
     @NonNull
     public static <T> Parser<String, T> createStringParser(@NonNull Class<T> type) {
         return createStringParser(new ObjectMapper(), type);
+    }
+
+    /**
+     * Returns a new Parser which parses from {@link BufferedSource} to the specified type, using
+     * the provided {@link JsonFactory} instance.
+     */
+    @NonNull
+    public static <T> Parser<BufferedSource, T> createSourceParser(@NonNull JsonFactory jsonFactory,
+                                                                   @NonNull Type type) {
+        Preconditions.checkNotNull(jsonFactory, "jsonFactory cannot be null.");
+        Preconditions.checkNotNull(type, "type cannot be null.");
+        return new JacksonSourceParser<T>(jsonFactory, type);
+    }
+
+    /**
+     * Returns a new Parser which parses from {@link BufferedSource} to the specified type, using
+     * the provided {@link ObjectMapper} instance.
+     */
+    @NonNull
+    public static <T> Parser<BufferedSource, T> createSourceParser(@NonNull ObjectMapper objectMapper,
+                                                                   @NonNull Type type) {
+        Preconditions.checkNotNull(objectMapper, "objectMapper cannot be null.");
+        Preconditions.checkNotNull(type, "type cannot be null.");
+        return new JacksonSourceParser<T>(objectMapper, type);
+    }
+
+    /**
+     * Returns a new Parser which parses from {@link BufferedSource} to the specified type, using
+     * a new default configured {@link ObjectMapper} instance.
+     */
+    @NonNull
+    public static <T> Parser<BufferedSource, T> createSourceParser(@NonNull Type type) {
+        return createSourceParser(new ObjectMapper(), type);
     }
 }
