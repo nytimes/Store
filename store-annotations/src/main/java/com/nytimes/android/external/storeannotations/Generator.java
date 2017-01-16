21620a1f17dafc09514100c7f8d2db27351ee44f
digitalbuddha <mnakhimovich@gmail.com>
2017-01-16 10:34:30 -0500

    capitilization

--- store-annotations/src/main/java/com/nytimes/android/external/storeannotations/Generator.java
+++ store-annotations/src/main/java/com/nytimes/android/external/storeannotations/Generator.java
@@ -107,7 +107,7 @@ public class Generator {
 
     public static String capitalize(String s) {
         if (s.length() == 0) return s;
-        return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
     }
 
 
