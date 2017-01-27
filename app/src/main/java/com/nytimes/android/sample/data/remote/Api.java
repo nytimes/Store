46e7def64d9bf5dcefa0ebc4ee8669683197f9db
digitalbuddha <mnakhimovich@gmail.com>
2017-01-27 09:45:25 -0500

    fix errors

--- app/src/main/java/com/nytimes/android/sample/data/remote/Api.java
+++ app/src/main/java/com/nytimes/android/sample/data/remote/Api.java
@@ -2,7 +2,6 @@ package com.nytimes.android.sample.data.remote;
 
 import com.nytimes.android.sample.data.model.RedditData;
 
-import okhttp3.Response;
 import okhttp3.ResponseBody;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
