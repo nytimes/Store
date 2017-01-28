08b7ea130a0bee3ce8930810b5ee33a5d7807b87
Brian Plummer <brian.r.plummer@gmail.com>
2017-01-28 11:54:37 -0500

    start of store conversion

--- store/src/main/java/com/nytimes/android/external/store/base/DiskRead.java
+++ store/src/main/java/com/nytimes/android/external/store/base/DiskRead.java
@@ -1,12 +1,12 @@
 package com.nytimes.android.external.store.base;
 
-import android.support.annotation.NonNull;
+import org.jetbrains.annotations.NotNull;
 
 import com.nytimes.android.external.store.base.impl.BarCode;
 
 import rx.Observable;
 
 public interface DiskRead<Raw> {
-    @NonNull
+    @NotNull
     Observable<Raw> read(BarCode barCode);
 }
