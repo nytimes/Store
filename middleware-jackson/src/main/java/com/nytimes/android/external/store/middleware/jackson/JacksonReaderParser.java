d89d759f468a879923a5d208cd4b3099bc2d429d
Brian Plummer <brian.r.plummer@gmail.com>
2017-01-30 21:08:19 -0500

    convert middleware-jackson to javax.annotations

--- middleware-jackson/src/main/java/com/nytimes/android/external/store/middleware/jackson/JacksonReaderParser.java
+++ middleware-jackson/src/main/java/com/nytimes/android/external/store/middleware/jackson/JacksonReaderParser.java
@@ -6,12 +6,11 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nytimes.android.external.store.base.Parser;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
 
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 
 public class JacksonReaderParser<Parsed> implements Parser<Reader, Parsed> {
@@ -19,19 +18,19 @@ public class JacksonReaderParser<Parsed> implements Parser<Reader, Parsed> {
     private final ObjectMapper objectMapper;
     private final JavaType parsedType;
 
-    public JacksonReaderParser(@NotNull JsonFactory jsonFactory, @NotNull Type type) {
+    public JacksonReaderParser(@Nonnull JsonFactory jsonFactory, @Nonnull Type type) {
         objectMapper = new ObjectMapper(jsonFactory);
         parsedType = objectMapper.constructType(type);
     }
 
     @Inject
-    public JacksonReaderParser(@NotNull ObjectMapper objectMapper, @NotNull Type type) {
+    public JacksonReaderParser(@Nonnull ObjectMapper objectMapper, @Nonnull Type type) {
         this.objectMapper = objectMapper;
         parsedType = objectMapper.constructType(type);
     }
 
     @Override
-    public Parsed call(@NotNull Reader reader) {
+    public Parsed call(@Nonnull Reader reader) {
         try {
             return objectMapper.readValue(reader, parsedType);
         } catch (IOException e) {
