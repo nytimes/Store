d6283a79d9aaa4ec9202f8f86d58c96022d55125
Paul Woitaschek <woitaschek@posteo.de>
2017-02-10 14:16:33 +0100

    Wrote a test for the RepeatWhenEmits transformer

---
+++ store/src/test/java/com/nytimes/android/external/store/base/impl/RepeatWhenEmitsTest.java
@@ -0,0 +1,41 @@
+package com.nytimes.android.external.store.base.impl;
+
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+
+import rx.Observable;
+import rx.observers.AssertableSubscriber;
+import rx.subjects.PublishSubject;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+public class RepeatWhenEmitsTest {
+
+    @Test
+    public void testTransformer() throws Exception {
+        // prepare mock
+        //noinspection unchecked
+        Callable<String> mockCallable = (Callable<String>) mock(Callable.class);
+        when(mockCallable.call()).thenReturn("value");
+
+        // create an observable and apply the transformer to test
+        PublishSubject<String> source = PublishSubject.create();
+        AssertableSubscriber<String> testSubscriber = Observable.fromCallable(mockCallable)
+                .compose(RepeatWhenEmits.<String>from(source))
+                .test();
+
+        for (int i = 1; i < 10; i++) {
+            // check that the original observable was called i times and that i events arrived.
+            verify(mockCallable, times(i)).call();
+            testSubscriber.assertValueCount(i);
+
+            source.onNext("event");
+        }
+    }
+
+}
