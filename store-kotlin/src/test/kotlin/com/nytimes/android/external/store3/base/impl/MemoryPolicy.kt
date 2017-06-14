f7916589025efbc73940bf28d19ac19f961f0601
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-06-14 21:44:55 +0200

    Remove useless file

--- store-kotlin/src/test/kotlin/com/nytimes/android/external/store3/base/impl/MemoryPolicy.kt
+++
@@ -1,9 +0,0 @@
-package com.nytimes.android.external.store3.base.impl
-
-//when(other) {
-//    is MemoryPolicy -> this.expireAfterWrite == other.expireAfterWrite &&
-//            this.expireAfterAccess == other.expireAfterAccess &&
-//            this.expireAfterTimeUnit == other.expireAfterTimeUnit &&
-//            this.maxSize == other.maxSize
-//    else -> false
-//}
