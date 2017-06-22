d748c2c67831ae9d521e29565ed9d1ca2d0cabd5
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-06-22 18:05:00 +0200

    Add Experimental annotation

---
+++ store/src/main/java/com/nytimes/android/external/store3/annotations/Experimental.java
@@ -0,0 +1,8 @@
+package com.nytimes.android.external.store3.annotations;
+
+/**
+ * Indicates the feature is in experimental state: its existence, signature or behavior
+ * might change without warning from one release to the next.
+ */
+public @interface Experimental {
+}
