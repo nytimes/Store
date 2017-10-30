aa68df01133e1ecd27d68f02c4a1820824346e94
Mateusz Perlak <Mat.Perlak@outcomehealth.com>
2017-10-30 15:49:22 -0500

    fix for checkstyle issues - end new line and max size of line

--- store/src/main/java/com/nytimes/android/external/store3/base/impl/ParsingFetcher.java
+++ store/src/main/java/com/nytimes/android/external/store3/base/impl/ParsingFetcher.java
@@ -31,7 +31,8 @@ public class ParsingFetcher<Parsed, Raw, Key> implements Fetcher<Parsed, Key> {
     /**
      * Creates ParsingFetcher for raw data type Fetcher and Raw data Parser.
      */
-    public static final <Parsed, Raw, Key> ParsingFetcher<Parsed, Raw, Key> from(@Nonnull Fetcher<Raw, Key> fetcher, @Nonnull Parser<Raw, Parsed> parser) {
+    public static final <Parsed, Raw, Key> ParsingFetcher<Parsed, Raw, Key> from(
+            @Nonnull Fetcher<Raw, Key> fetcher, @Nonnull Parser<Raw, Parsed> parser) {
         return new ParsingFetcher(fetcher, parser);
     }
 
