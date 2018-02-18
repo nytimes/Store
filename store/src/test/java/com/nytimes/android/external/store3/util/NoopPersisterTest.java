f50bf4ae01174ff9307cdf8c3f674f5fcb4c87f8
Saket Narayan <saket@saket.me>
2018-02-18 17:50:19 +0530

    Add regression test for #312

--- store/src/test/java/com/nytimes/android/external/store3/util/NoopPersisterTest.java
+++ store/src/test/java/com/nytimes/android/external/store3/util/NoopPersisterTest.java
@@ -1,17 +1,22 @@
 package com.nytimes.android.external.store3.util;
 
 import com.nytimes.android.external.store3.base.impl.BarCode;
+import com.nytimes.android.external.store3.base.impl.MemoryPolicy;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.util.concurrent.TimeUnit;
 
 public class NoopPersisterTest {
 
-    private final BarCode barCode = new BarCode("key", "value");
+    @Rule public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void writeReadTest() {
+        BarCode barCode = new BarCode("key", "value");
         NoopPersister<String, BarCode> persister = NoopPersister.create();
         boolean success = persister.write(barCode, "foo").blockingGet();
         assertThat(success).isTrue();
@@ -23,10 +28,32 @@ public class NoopPersisterTest {
     public void noopParserFuncTest() {
         NoopParserFunc<String, String> noopParserFunc = new NoopParserFunc<>();
         String input = "foo";
-        String output = (String) noopParserFunc.apply(input);
+        String output = noopParserFunc.apply(input);
         assertThat(input).isEqualTo(output);
         //intended object ref comparison
         assertThat(input).isSameAs(output);
     }
 
+    // https://github.com/NYTimes/Store/issues/312
+    @Test
+    public void testReadingOfMemoryPolicies() {
+        MemoryPolicy expireAfterWritePolicy = MemoryPolicy.builder()
+            .setExpireAfterWrite(1)
+            .setExpireAfterTimeUnit(TimeUnit.HOURS)
+            .build();
+        NoopPersister.create(expireAfterWritePolicy);
+
+        MemoryPolicy expireAfterAccessPolicy = MemoryPolicy.builder()
+            .setExpireAfterAccess(1)
+            .setExpireAfterTimeUnit(TimeUnit.HOURS)
+            .build();
+        NoopPersister.create(expireAfterAccessPolicy);
+
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("No expiry policy set");
+        MemoryPolicy incompletePolicy = MemoryPolicy.builder()
+            .setExpireAfterTimeUnit(TimeUnit.HOURS)
+            .build();
+        NoopPersister.create(incompletePolicy);
+    }
 }
