b0a6bc25b742574a3cabf0ee203eeecc6e616600
Brian Plummer <brian.plummer@nike.com>
2018-07-18 23:46:15 +0200

    start of filesystem impl conversion to kotlin

---
+++ filesystem/src/main/java/com/nytimes/android/external/fs3/RecordPersister.kt
@@ -0,0 +1,28 @@
+package com.nytimes.android.external.fs3
+
+import com.nytimes.android.external.fs3.filesystem.FileSystem
+import com.nytimes.android.external.store3.base.RecordProvider
+import com.nytimes.android.external.store3.base.RecordState
+import com.nytimes.android.external.store3.base.impl.BarCode
+
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class RecordPersister @Inject
+constructor(fileSystem: FileSystem,
+            private val expirationDuration: Long,
+            private val expirationUnit: TimeUnit) : SourcePersister(fileSystem), RecordProvider<BarCode> {
+
+    override fun getRecordState(barCode: BarCode): RecordState {
+        return sourceFileReader.getRecordState(barCode, expirationUnit, expirationDuration)
+    }
+
+    companion object {
+
+        fun create(fileSystem: FileSystem,
+                   expirationDuration: Long,
+                   expirationUnit: TimeUnit): RecordPersister {
+            return RecordPersister(fileSystem, expirationDuration, expirationUnit)
+        }
+    }
+}
