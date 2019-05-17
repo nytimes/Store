797b3e2555f3062f38d2b986d90fb75acf99df11
Brian Plummer <brian.r.plummer@gmail.com>
2019-05-17 18:30:24 -0400

    remove middleware-jackson

--- middleware-jackson/src/test/java/com/nytimes/android/external/store3/middleware/jackson/Model.kt
+++
@@ -1,9 +0,0 @@
-package com.nytimes.android.external.store3.middleware.jackson
-
-data class Bar (val string: String)
-
-data class Foo (
-        val number: Int,
-        val string: String,
-        val bars: List<Bar>
-)
